53febdc5afba122542a520bf066d3b2f14b7d0b0
cristobalwalters12 <cristobalwalters@gmail.com>
2023-08-03 21:15:47 -0400

    ejercicio 3 Realizado

--- assest/js/index.js
+++ assest/js/index.js
@@ -7,4 +7,20 @@ function toggle() {
   }
 }
 
-function ingresar() {}
+function ingresarPassword() {
+  if (
+    document.getElementById("select1").value == 9 &&
+    document.getElementById("select2").value == 1 &&
+    document.getElementById("select3").value == 1
+  ) {
+    document.getElementById("paragraph").innerHTML = "Password 1 correcto";
+  } else if (
+    document.getElementById("select1").value == 7 &&
+    document.getElementById("select2").value == 1 &&
+    document.getElementById("select3").value == 4
+  ) {
+    document.getElementById("paragraph").innerHTML = "Password 2 Correcta";
+  } else {
+    document.getElementById("paragraph").innerHTML = "Password Incorrecta";
+  }
+}
