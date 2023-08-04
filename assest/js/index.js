4eadc78de5fd007e7b64253c021820f3112dd734
cristobalwalters12 <cristobalwalters@gmail.com>
2023-08-03 20:55:25 -0400

    Ejercicio 1 Terminado

---
+++ assest/js/index.js
@@ -0,0 +1,10 @@
+function toggle() {
+  const imagen = document.getElementById("miImagen");
+  if (imagen.style.border === "2px solid red") {
+    imagen.style.border = "";
+  } else {
+    imagen.style.border = "2px solid red";
+  }
+}
+
+function ingresar() {}
